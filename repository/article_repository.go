1f9d4a24f3792ca4fcf686e3c397f65a24db6ca2
Joseph Cha <37521249+josephcha4@users.noreply.github.com>
2021-08-14 23:27:05 +1000

    update article repository

--- repository/article_repository.go
+++ repository/article_repository.go
@@ -22,10 +22,15 @@ func NewArticleRepository(db *sql.DB) articleRepository {
 }
 
 func (a articleRepository) Get(id int) (article model.Article, err error) {
-	var tags string
-	err = a.DB.QueryRow(
-		"SELECT a.id, a.title, a.body, a.date, group_concat(t.name) as tags FROM article as a INNER JOIN tag as t ON a.id = t.articleId WHERE a.id = ?", id,
-	).Scan(&article.ID, &article.Title, &article.Body, &article.Date, &tags)
-	article.Tags = strings.Split(tags, ",")
+	var tags sql.NullString
+	err = a.DB.QueryRow(`
+		SELECT a.id, a.title, a.date, a.body, group_concat(t.name) as tags 
+		FROM article AS a 
+		LEFT JOIN tag AS t 
+		ON a.id = t.article_id
+		WHERE a.id = ?
+		GROUP BY a.id
+	`, id).Scan(&article.ID, &article.Title, &article.Date, &article.Body, &tags)
+	article.Tags = strings.Split(tags.String, ",")
 	return
 }
